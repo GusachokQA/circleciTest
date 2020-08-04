0d9b873f25b73a74988e08f202b4b2185fe5c02f
maryia.bartosh <maryia.bartosh@vizor-games.com>
2020-08-04 20:13:04 +0300

    init test creations

---
+++ src/test/java/org/teachmeskills/aqa5/MainPageTest.java
@@ -0,0 +1,27 @@
+package org.teachmeskills.aqa5;
+
+import io.github.bonigarcia.wdm.DriverManagerType;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainPageTest {
+    WebDriver driver;
+
+    @Test
+    public void openChromePage(){
+        System.out.println("Selenium test has been started!");
+        DriverManagerType driverManagerType = DriverManagerType.CHROME;
+        WebDriverManager.getInstance(driverManagerType).setup();
+
+
+        driver = new ChromeDriver();
+        driver.get("http://google.com");
+
+        System.out.println(driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), "Google");
+        driver.close();
+    }
+}
